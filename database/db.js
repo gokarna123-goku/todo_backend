2e7a61f6d3843e9a61fc2462ef3740e4f8c7b930
gokarna123-goku <agokarna444@gmail.com>
2023-07-22 17:02:52 +0545

    created database

---
+++ database/db.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const Connection = () => {
+  const MONGODB_URI =
+    "mongodb+srv://logi2065:<logi12345>@cluster0.wdixa4s.mongodb.net/?retryWrites=true&w=majority";
+
+  mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
+
+  mongoose.connection.on("connected", () => {
+    console.log("Database connected successfully");
+  });
+
+  mongoose.connection.on("disconnected", () => {
+    console.log("Database disconnected successfully");
+  });
+
+  mongoose.connection.on("error", () => {
+    console.log("Error occured while connecting database", error.message);
+  });
+};
+
+export default Connection;
