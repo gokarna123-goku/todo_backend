f4572c84996b437ffc11aca1eb317de5bd2599ee
gokarna123-goku <agokarna444@gmail.com>
2023-07-24 09:14:17 +0545

    save data to the database

--- controller/todo-controller.js
+++ controller/todo-controller.js
@@ -1,5 +1,14 @@
+import Todo from "../model/Todo.js";
 
-
-export const addTodo = (req, res) => {
-    console.log(req.body);
-}
+export const addTodo = async (req, res) => {
+    try{
+        const newTodo = await Todo.create({
+            data: req.body.data,
+            createdAt: Date.now(),
+        });
+        await newTodo.save();
+        res.status(200).json(newTodo);
+    } catch (error){
+        return res.status(500).json(error.message)
+    }
+};
---
+++ model/Todo.js
@@ -0,0 +1,20 @@
+import mongoose, { model } from "mongoose";
+
+const TodoSchema = new mongoose.Schema({
+    data:{
+        type: String,
+        required: true
+    },
+    done: {
+        type: Boolean,
+        default: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const todo = mongoose.model('todo', TodoSchema);
+
+export default todo;
